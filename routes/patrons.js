838d1d52f4ae6218e2ab14207290e2ef7207bb52
KaitlynDodds <kadodds.dev@kadodds.com>
2018-10-08 12:57:16 -0600

    added basic patron routes

--- routes/patrons.js
+++ routes/patrons.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'All Patrons' });
 });
 
+/* GET Patron Detail page (form) */
+router.get('/:id/detail', function(req, res, next) {
+    res.render('index', { title: 'Patron Detail Page' });
+});
+
+/* PUT Update Patron */
+router.put('/:id', function(req, res, next) {
+    // UPDATE patron in db
+});
+
+/* GET New Patron page (form) */
+router.get('/new', function(req, res, next) {
+    res.render('index', { title: 'New Patron Page' });
+});
+
+/* PUSH Create New Patron */
+router.push('/', function(req, res, next) {
+    // CREATE new patron in db
+});
+
 module.exports = router;
