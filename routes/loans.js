d3c4fce377616d970dc34bc76514d6d7276fb71e
KaitlynDodds <kadodds.dev@kadodds.com>
2018-10-08 13:03:31 -0600

    adding basic loan routes

--- routes/loans.js
+++ routes/loans.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET All LOANS page. */
 router.get('/', function(req, res, next) {
+    // TODO: filter by 'all', 'overdue', 'checked out'
   res.render('index', { title: 'All Loans' });
+});
+
+/* GET New Loan page (form) */
+router.get('/new', function(req, res, next) {
+  res.render('index', { title: 'New Loan' });
+});
+
+/* PUSH New Loan */
+router.push('/', function(req, res, next) {
+    // CREATE new loan in db
 });
 
 module.exports = router;
