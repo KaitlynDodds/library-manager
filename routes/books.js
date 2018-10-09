458d78c5809dc164c24277eb95166cbee32c04a9
KaitlynDodds <kadodds.dev@kadodds.com>
2018-10-09 10:53:09 -0600

    /books route working, fetching from db based on filter option

--- routes/books.js
+++ routes/books.js
@@ -1,16 +1,65 @@
 var express = require('express');
 var router = express.Router();
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 // models
 const Book = require('../models').Book;
+const Loan = require('../models').Loan;
+
+function getCheckedOutBooksQuery() {
+    return {
+        attributes: ['title', 'author', 'genre', 'first_published', 'id'],
+        include: [{
+            model: Loan,
+            where: { 
+                book_id: Sequelize.col('book.id'),
+                // book has not been returned yet 
+                returned_on: {
+                    [Op.eq]: null
+                }
+            },
+            attributes: [['id', 'loan_id']]
+        }]
+    }
+}
+
+function getOverdueBooksQuery() {
+    return {
+        attributes: ['title', 'author', 'genre', 'first_published', 'id'],
+        include: [{
+            model: Loan,
+            where: { 
+                book_id: Sequelize.col('book.id'),
+                // less than today's date
+                return_by: {
+                    [Op.lt]: new Date() 
+                },
+                // returned_on is null (book has not been returned)
+                returned_on: {
+                    [Op.eq]: null
+                }
+            },
+            attributes: [['id', 'loan_id']]
+        }]
+    }
+}
 
 /* GET All BOOKS page. */
 router.get('/', function(req, res, next) {
-    // TODO: Filter by 'all', checked out', 'overdue'
-    Book.findAll()
+    // default query (get all books)
+    let query = {};
+
+    if (req.query.filter === 'checked_out') {
+        query = getCheckedOutBooksQuery();
+    } else if (req.query.filter === 'overdue') {
+        query = getOverdueBooksQuery();
+    }   
+
+    Book.findAll(query)
         .then((books) => {
             if (books) {
-                res.render('books', { books, title: 'Books' });
+                res.render('books', { books, title: 'Books', filter: req.query.filter });
             } else {
                 res.send(404);
             }
