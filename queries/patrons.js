b839aa8772d88c31f2f165c1a1b8aebe22b0fbaf
KaitlynDodds <kadodds.dev@kadodds.com>
2018-10-09 11:58:09 -0600

    add query to fetch all patrons

---
+++ queries/patrons.js
@@ -0,0 +1,11 @@
+const Sequelize         = require('sequelize');
+const Op                = Sequelize.Op;
+
+// models
+const Patron        = require('../models').Patron;
+
+
+
+module.exports = {
+    
+}
--- routes/patrons.js
+++ routes/patrons.js
@@ -1,29 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
+// models
+const Patron        = require('../models').Patron;
+
 /* GET All PATRONS page. */
 router.get('/', function(req, res, next) {
-  res.render('patrons', { title: 'Patrons' });
+	Patron.findAll()
+		.then((patrons) => {
+			if (patrons) {
+				res.render('patrons', { 
+					patrons,
+					title: 'Patrons'
+				});
+			} else {
+				res.send(404);
+			}
+		})
 });
 
 /* GET Patron Detail page (form) */
 router.get('/:id/detail', function(req, res, next) {
-    res.render('patron_detail', { title: 'Patron: {Patron Name}' });
+	res.render('patron_detail', { title: 'Patron: {Patron Name}' });
 });
 
 /* PUT Update Patron */
 router.put('/:id', function(req, res, next) {
-    // UPDATE patron in db
+	// UPDATE patron in db
 });
 
 /* GET New Patron page (form) */
 router.get('/new', function(req, res, next) {
-    res.render('patron_new', { title: 'New Patron' });
+	res.render('patron_new', { title: 'New Patron' });
 });
 
 /* POST Create New Patron */
 router.post('/', function(req, res, next) {
-    // CREATE new patron in db
+	// CREATE new patron in db
 });
 
 module.exports = router;
